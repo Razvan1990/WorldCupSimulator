a03b5a504bab04c77481ee43859fb2a14a102037
Razvan1990 <53650135+Razvan1990@users.noreply.github.com>
2022-11-14 10:39:47 +0200

    Refactoring to include goals received by each team
    Changed methods to include the goals received from every country

--- src/statistics/GoalsAndStrikers.java
+++ src/statistics/GoalsAndStrikers.java
@@ -4,6 +4,7 @@ import constants.Constants;
 import winner.GroupAndKnockoutPhaseWinners;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 
@@ -17,22 +18,23 @@ public class GoalsAndStrikers {
         statisticsHelper = new StatisticsHelper();
     }
 
-    public LinkedHashMap<String, Integer> computeNumberOfGoalsPerTeam() throws InterruptedException {
+    public LinkedHashMap<String, List<Integer>> computeNumberOfGoalsPerTeam() throws InterruptedException {
         /*
          * Method involves looking in every list created form Groupknockout and check to see if that team has scored goals
          * Based on this, we create a list of all goals from teams based on a list of teams from alphabetical order
          */
         try {
-            List<List<LinkedHashMap<String, Integer>>> last16Teams = groupAndKnockoutPhaseWinners.createLast16Teams();
-            List<List<LinkedHashMap<String, Integer>>> last8Teams = groupAndKnockoutPhaseWinners.getQuarterFinalTeams(last16Teams);
-            List<List<LinkedHashMap<String, Integer>>> last4Teams = groupAndKnockoutPhaseWinners.getSemifinalTeams(last8Teams);
-            List<List<LinkedHashMap<String, Integer>>> finals = groupAndKnockoutPhaseWinners.concludeFinals(last4Teams);
-            List<List<LinkedHashMap<String, Integer>>> winners = groupAndKnockoutPhaseWinners.getWinners(finals);
+            List<List<LinkedHashMap<String, List<Integer>>>> last16Teams = groupAndKnockoutPhaseWinners.createLast16Teams();
+            List<List<LinkedHashMap<String, List<Integer>>>> last8Teams = groupAndKnockoutPhaseWinners.getQuarterFinalTeams(last16Teams);
+            List<List<LinkedHashMap<String, List<Integer>>>> last4Teams = groupAndKnockoutPhaseWinners.getSemifinalTeams(last8Teams);
+            List<List<LinkedHashMap<String, List<Integer>>>> finals = groupAndKnockoutPhaseWinners.concludeFinals(last4Teams);
+            List<List<LinkedHashMap<String, List<Integer>>>> winners = groupAndKnockoutPhaseWinners.getWinners(finals);
             List<String> worldCupTeams = statisticsHelper.createListOfTeams();
-            List<Integer> goals = statisticsHelper.createListOfGoals(last16Teams, last8Teams, last4Teams, finals, winners, worldCupTeams);
-            LinkedHashMap<String, Integer> nationGoals = new LinkedHashMap<>();
-            for (int i = 0; i < goals.size(); i++) {
-                nationGoals.put(worldCupTeams.get(i), goals.get(i));
+            List<Integer> goalsScored = statisticsHelper.createListOfGoalsScored(last16Teams, last8Teams, last4Teams, finals, winners, worldCupTeams);
+            List<Integer> goalsReceived = statisticsHelper.createListOfGoalsReceived(last16Teams, last8Teams, last4Teams, finals, winners, worldCupTeams);
+            LinkedHashMap<String, List<Integer>> nationGoals = new LinkedHashMap<>();
+            for (int i = 0; i < goalsScored.size(); i++) {
+                nationGoals.put(worldCupTeams.get(i), Arrays.asList(goalsScored.get(i), goalsReceived.get(i)));
             }
 
             return nationGoals;
@@ -42,13 +44,13 @@ public class GoalsAndStrikers {
         return null;
     }
 
-    public LinkedHashMap<String, Integer> createPlayerGoalsScoredMap(LinkedHashMap<String, Integer> nationsGoalMap) {
+    public LinkedHashMap<String, Integer> createPlayerGoalsScoredMap(LinkedHashMap<String, List<Integer>> nationsGoalMap) {
         /*
          * Method involves taking the list of players and generating a random number of goals for every player based on the amount of goals scored by his country
          */
 
         LinkedHashMap<String, Integer> goalsPerPlayer = new LinkedHashMap<>();
-        List<Integer> goals = new ArrayList<>(nationsGoalMap.values());
+        List<Integer> goals = statisticsHelper.getGoalsScoredByTeam(nationsGoalMap, 0);
         List<Integer> randomPlayerGoals = statisticsHelper.getPlayerGoalNumber(goals);
         for (int i = 0; i < Constants.PLAYERS.size(); i++) {
             goalsPerPlayer.put(Constants.PLAYERS.get(i), randomPlayerGoals.get(i));
